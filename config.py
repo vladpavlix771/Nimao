ab50694f618e4802159b753b63a7087220d4598a
vladpavlix771 <56918679+vladpavlix771@users.noreply.github.com>
2022-04-15 10:11:46 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+#Токен вашего бота
+bot_token = '5323110502:AAEfmpiyQwZknjHmFtyQQoKGdo7R7epfR8s'
+#ID админа
+admin = 1370967532
+#Юзер саппорта
+support = '@blackToT_support'
+#Канал с отзывами
+#feedback = 'https://t.me/Black_ToT_feedback'
+#Название магазина
+shop_name = 'BlackToT'
+
+#Скрипт скачан с канала https://t.me/RabotaDotOnion 
